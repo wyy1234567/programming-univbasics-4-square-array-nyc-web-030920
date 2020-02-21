caeabfb248c12093588b1b0ad5b89e034b07bdb5
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-21 17:20:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  counter = 0 
+  size = array.size
+  newArr = []
+  
+  while counter < size do
+    num = array[counter] ** 2 
+    newArr.push(num)
+    counter += 1 
+  end
+  newArr
 end
